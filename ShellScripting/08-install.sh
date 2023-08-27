7658c4ed237eff445e28f41d22d2851d3cb019e1
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 12:11:58 -0500

    install application

---
+++ ShellScripting/08-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Our program goal is to install mqsql
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then 
+    echo "ERROR:: Please run this script with root access"
+
+#else
+#   echo "INFO:: Your are root user"
+fi
+
+yum install nginx -y
