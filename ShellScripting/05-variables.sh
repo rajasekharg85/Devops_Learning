4b60eeff9fa14e9fbe908e3a82b873253abf9065
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 00:04:49 -0500

    added username input

--- ShellScripting/05-variables.sh
+++ ShellScripting/05-variables.sh
@@ -4,4 +4,4 @@ echo "Please enter your username"
 
 read  USERNAME # this is the variable where the value of the one user entered 
 
-echp "Username entered is $USERNAME"
+echp "Username entered is: $USERNAME"
