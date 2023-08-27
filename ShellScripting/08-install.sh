cf39d7e30b8958d8d9d3bbac5d1f8d9a7e8995e9
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 12:33:44 -0500

    install application

--- ShellScripting/08-install.sh
+++ ShellScripting/08-install.sh
@@ -21,3 +21,12 @@ then
 else 
     echo "Installation of mysql is success"
 fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of postfix is error"
+else 
+    echo "Installation of postfix is success"
+fi
