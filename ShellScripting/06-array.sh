d0e95f47f87966539881278e795aec9001cac8ac
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 00:19:52 -0500

    array example

--- ShellScripting/06-array.sh
+++ ShellScripting/06-array.sh
@@ -3,4 +3,5 @@
 # This is called array, instead of single value it can hold multiple values
 PERSONS=("Ramesh" "Suresh" "Rajesh")
 # Inside array we always refer values with index and it starts from 0
-echo "First Person: ${PERSONS[@]}"
+echo "First Person: ${PERSONS[0]}"
+echo "All Person: ${PERSONS[@]}"
