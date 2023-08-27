ef2be26235e5f789d44bc2161c96a5d1a7d08435
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 00:34:54 -0500

    conditions added

---
+++ ShellScripting/07-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if the bumber is greater than 10 or not
+
+NUMBER=$1
+
+if [ $NUMNER -gt 10 ]
+then 
+    echo "$NUMBER is greter then 10"
+else
+    echo "NUMBER is not greter then 10"
+fi
