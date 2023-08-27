aec22d2ed2e623fdc0739bc41d81d4ad8aac65da
Rajasekhar <rajasekharg85@gmail.com>
2023-08-26 23:37:59 -0500

    updating user comments

--- ShellScripting/01-hello-world.sh
+++ ShellScripting/01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World, I am learning Devops with AWS"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good, how are you Ramesh"
