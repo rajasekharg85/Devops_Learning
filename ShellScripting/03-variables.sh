d8e83c18a92946cd29f5a2af9226fb4a6c124469
Rajasekhar <rajasekharg85@gmail.com>
2023-08-26 23:54:53 -0500

    input values to variable

---
+++ ShellScripting/03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello World, I am learning Devops with AWS"
+
+PERSON1=$1 # This is the first value from command line
+PERSON2=$2 # This is the second value from command line
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON2"
