857ee8a747f8fcd8e7df1f3eae038215ebafc067
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 20:02:21 -0500

    loops

--- ShellScripting/13-delete-old-logs.sh
+++ ShellScripting/13-delete-old-logs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 APP_LOGS_DIR=/app-logs
-
 DATE=$(date +%F)
 LOGDIR=/home/centos/shellscripts-logs
 SCRIPT_NAME=$0
@@ -9,3 +8,10 @@ LOGFILE=$LOGDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+while read line
+do
+    echo "Deleting $line" &> $LOGFILE
+    rm -rf $line
+    ## We write our statement here
+done < $FILES_TO_DELETE
