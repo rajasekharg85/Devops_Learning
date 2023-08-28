a375048ae590dbb0535077d574c0cff5ce6338d0
Rajasekhar <rajasekharg85@gmail.com>
2023-08-27 20:18:26 -0500

    loops

--- ShellScripting/13-delete-old-logs.sh
+++ ShellScripting/13-delete-old-logs.sh
@@ -7,6 +7,8 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
+echo "script started executing at $DATE"
+
 echo "$FILES_TO_DELETE"
 
 while read line
