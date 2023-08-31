6bbecb16626544c649b287b1e46623561c948efd
Rajasekhar <rajasekharg85@gmail.com>
2023-08-31 12:26:05 -0500

    Disk usage

--- ShellScripting/disk_usage.sh
+++ ShellScripting/disk_usage.sh
@@ -14,7 +14,7 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-DISK_USAGE=(df -hT | grep -vE "tmpfs|Filesystem")
+DISK_USAGE=$(df -hT | grep -vE "tmpfs|Filesystem")
 DISK_USAGE_THRESHOLD=1
 
 #IFS means internal field separator is space
