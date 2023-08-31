041aac98e860b76d2558c340f7dc1d09569d6cf0
Rajasekhar <rajasekharg85@gmail.com>
2023-08-31 12:36:11 -0500

    Disk usage

--- ShellScripting/disk_usage.sh
+++ ShellScripting/disk_usage.sh
@@ -20,5 +20,15 @@ DISK_USAGE_THRESHOLD=1
 #IFS means internal field separator is space
 while IFS= read line
 do
-    echo "ouput: $line"
+    # This command will give you usage in number format for comparision
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    ## This command will give us partition
+    partition=$(echo $line | awk '{print $1}')
+    ## Now you need to check whether it is more then threshhold or not
+    if [$usage -gt $DISK_USAGE_THRESHOLD];
+    then
+        message+="HIGH DISK USAGE on $partition: $usage"
+    fi
 done <<< $DISK_USAGE
+
+echo "message: $message"
