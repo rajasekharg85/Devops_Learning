178b70b8a894eb208ddfd98dbd115d5f9f613b44
Rajasekhar <rajasekharg85@gmail.com>
2023-08-31 14:59:37 -0500

    Roboshop Shell

--- roboshop-shell/mango.sh
+++ roboshop-shell/mango.sh
@@ -12,14 +12,14 @@ G="\e[33m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ USERID -ne 0];
+if [ $USERID -ne 0];
 then
     echo -e "$R ERROR: Please run this script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
-if [$1 ne 0];
+if [$1 -ne 0];
 then
     echo -e "$2 ... $R FAILURE $N"
     exit 1
