9e04ae0e159eb642b8ea4e1dad4fa576f6981bfe
Rajasekhar <rajasekharg85@gmail.com>
2024-03-24 14:55:36 -0500

    Roboshop Shell to create multiple instances

--- Ansible/Ec2-install.sh
+++ Ansible/Ec2-install.sh
@@ -2,15 +2,18 @@
 
 NAMES=("mandodb" "redis" "mysql" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
+IMAGE_ID=ami-0f3c7d07486cad139
+SECURITY_GROUP_ID=sg-0ac489c6167a83372
 #if mysql or mongodb instanse_type should be t3.medium. for all others it is t2.micro
 
 for i in "${NAMES[@]}"
 do
-    if [ [ $i == "mangodb" || $i = "mysql" ]]
+    if [ [ $i == "mangodb" || $i == "mysql" ] ];
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    echo "Name: is $i"
+    echo "Creating $i instance"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
