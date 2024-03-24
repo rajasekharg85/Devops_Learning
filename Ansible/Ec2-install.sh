d19d15a4501d5870f61ea1aeea67b9cd3fb85547
Rajasekhar <rajasekharg85@gmail.com>
2024-03-24 15:03:35 -0500

    Roboshop Shell to create multiple instances

--- Ansible/Ec2-install.sh
+++ Ansible/Ec2-install.sh
@@ -15,5 +15,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "Creating $i instance"
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "Create $i instance: $IP_ADDRESS"
 done
