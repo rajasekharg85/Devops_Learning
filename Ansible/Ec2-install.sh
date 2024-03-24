a3dc11f077edcfe6db6e544e6712cb78474fbdf2
Rajasekhar <rajasekharg85@gmail.com>
2024-03-24 14:52:41 -0500

    Roboshop Shell to create multiple instances

--- Ansible/Ec2-install.sh
+++ Ansible/Ec2-install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 NAMES=("mandodb" "redis" "mysql" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+#if mysql or mongodb instanse_type should be t3.medium. for all others it is t2.micro
+
 for i in "${NAMES[@]}"
 do
+    if [ [ $i == "mangodb" || $i = "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
     echo "Name: is $i"
 done
