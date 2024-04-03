5e2d4e3d72975b690652348e290a579967485fb2
Rajasekhar <rajasekharg85@gmail.com>
2024-04-03 16:10:38 -0500

    Ansible Basics

--- Ansible/create-servers.sh
+++ Ansible/create-servers.sh
@@ -8,19 +8,21 @@ DOMAIN_NAME=rajus.cloud
 HOSTED_ZONE_ID=Z0153013VMDT5YYMVHI7
 #if mysql or mongodb instanse_type should be t3.medium. for all others it is t2.micro
 
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
 for i in $@
-do
-    if [[ $i == "mangodb" || $i == "mysql" ]]
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    echo "Creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "Created $i instance: $IP_ADDRESS"
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z0153013VMDT5YYMVHI7 --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
@@ -28,8 +30,8 @@ do
                             "Name": "'$i.$DOMAIN_NAME'",
                             "Type": "A",
                             "TTL": 300,
-                            "ResourceRecords": [{ "Value: "'$IP_ADDRESS'"}]
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
                         }}]
-        }
-        '
-    done  
+    }
+    '
+done
